7c032a7a1cfa445deadeb6fc52aea3c13becb44a
Victor Donet Peris <victordp86@gmail.com>
2024-08-22 05:49:29 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='utils_package',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+)
 RECALCULATE_TABLE = os.getenv('ETL_RECALCULATE_DB_TABLE', 'sc_valencia.immission_recalculation')
